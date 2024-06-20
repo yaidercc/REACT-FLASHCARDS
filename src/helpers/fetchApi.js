b46af398403bd463ccd07b74a9b9eccc4ec56b6a
Rediat <yaiderc19@gmail.com>
2024-06-20 16:13:08 -0500

    adding env

--- src/helpers/fetchApi.js
+++ src/helpers/fetchApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { API_URL } from "../config/config"
+
 
 const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.API_URL,
   headers:{
     "Content-Type": "application/json"
   }
